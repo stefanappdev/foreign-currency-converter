f2278b349aaa9bb372713ba75de653fb5aaaba33
stefanappdev <stevendking@protonmail.com>
2026-01-10 18:48:48 -0800

    testing free currency API

---
+++ src/App.d.ts
@@ -0,0 +1,4 @@
+import './styles/App.css';
+declare function App(): import("react/jsx-runtime").JSX.Element;
+export default App;
+//# sourceMappingURL=App.d.ts.map
--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,143 @@
-import { useState } from 'react'
+import { useState,useEffect,useRef, use } from 'react'
 import './styles/App.css'
 
-function App() {
- 
 
+
+
+ function App() {
+
+const [baseCurrency,setBaseCurrency]=useState('');
+const [destCurrency,setDestCurrency]=useState('');
+const [baseCurrencyValue,setBaseCurrencyValue]=useState(null);
+const [destCurrencyValue,setDestCurrencyValue]=useState(null);
+const [rates,setRates]=useState(null);
+
+ useEffect(()=>{
+        
+        fetchCurrencyAPI(apiURL,baseCurrency)
+      
+      },[baseCurrency])
+    
+
+
+const handleSrcCurrencyChange=(e)=>{
+    if(e.target.value===''){
+      return
+    }else{
+      setBaseCurrency(e.target.value)
+      console.log(rates)
+    }  
+     
+}
+
+const handleDestCurrencyChange=(e)=>{
+    if(e.target.value===''){
+      return
+    }else{
+      
+      
+  }
+}
+
+let apiURL='https://api.frankfurter.dev/v1/latest'
+
+const fetchCurrencyAPI= async (url:string,src_currency?:string)=>{
+  
+  
+  
+  src_currency? url=url+`?base=${src_currency}`:src_currency;  
+  const response=await fetch(url);  
+  try{ 
+      if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+            }else{
+                      
+                response.json().then(data=>{
+                    setRates(data.rates)
+                        })
+                }
+
+    }catch(error){
+                  console.error(error)
+    }           
+
+  }
+ 
   return (
-    <>
+    <div id='app'>
       <h1 id='app-title'>Foreign Currency Converter</h1>
       <span id='app-tagline'>Convert your foreign currencies here!</span>
       
       <div id='conversion-window'>
-       <span>choose your currencies to begin conversion</span>
 
-         <div>
+       
 
-          <label htmlFor='source-currency'>From:</label>
-            <select id='source-currency'>
-              <option>---SELECT---</option>
-              <option value='US'>US</option>
-              <option value='UK'>UK</option>
-              <option value='Canada'>Canadian</option>
-              <option value='China'>China</option>
-              <option value='Russia'>Russia</option>
+        
+
+         <div className='currency-input-container'>
+
+          <label htmlFor='source-currency'>
+            <strong>From: </strong>
+          </label>
+
+         
+
+            <select onChange={handleSrcCurrencyChange}  id='source-currency'>
+              <option value=''>---SELECT---</option>
+              <option value='USD'>US</option>
+              <option value='GBP'>British</option>
+              <option value='CAD'>Canadian</option>
+              <option value='CNY'>Chinese</option>
+              
             </select>
 
-            <input className='currency-inputs' placeholder='Enter an amount here'/>
+            
+
+            <input 
+              
+              value={baseCurrencyValue}
+              className='currency-inputs' 
+              type='text' 
+              placeholder=' $ 0.00'
+            />
         
            </div>
 
-           <div className='reversible-arrow-container'>
+           
 
-            <img id='reversible-arrow' alt='click here swap your currencies' src=''/>
+            <img id='reversible-arrow-img'  alt='click here swap your currencies'  src='public/images/reversible-arrow.svg'/> 
 
-           </div>
+          
+          
+          
+          
+          <div className='currency-input-container'>
 
-          <div>
-            <label htmlFor='destination-currency'>To:</label>
-              <select id='destination-currency'>
+            <label htmlFor='destination-currency'>
+               <strong>To: </strong>
+            </label>
+
+              <select onChange={handleDestCurrencyChange} id='destination-currency'>
                 <option>---SELECT---</option>
-                <option value='US'>US</option>
-                <option value='UK'>UK</option>
-                <option value='Canada'>Canadian</option>
-                <option value='China'>China</option>
-                <option value='Russia'>Russia</option>
+              <option value='USD'>US</option>
+              <option value='GBP'>British</option>
+              <option value='CAD'>Canadian</option>
+              <option value='CNY'>Chinese</option>
               </select>
 
-              <input className='currency-inputs' placeholder='Enter an amount here'/>
+              
+
+              <input 
+                className='currency-inputs' 
+                type='text' 
+                placeholder='$ 0.00'
+                value={destCurrencyValue}
+
+              />
             
           </div>
-      
       </div>
-    </>
+    </div>
   )
 }
 
